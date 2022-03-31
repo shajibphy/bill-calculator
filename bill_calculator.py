7bd0ffb8ffb826d882580a6d321355fe3a7369cf
Shafiul Alam <shajib.phy@gmail.com>
2022-03-31 17:56:29 +0300

    this is a modified code

--- bill_calculator.py
+++ bill_calculator.py
@@ -4,5 +4,5 @@ customer_number = input("What is the number of customers:")
 tip_percentage = input("put the tip percentage:")
 tip_total = float(tip_percentage)/100 + 1
 bill_per_person = float(total_bill)/float(customer_number)
-bill_with_tip = (bill_per_person*tip_total)
+bill_with_tip = round((bill_per_person*tip_total),2)
 print(bill_with_tip)
